76937735f1143d34af3cec39e12200d331cb1796
Joyesh <94055810+joiboi08@users.noreply.github.com>
2025-08-17 01:28:58 +0530

    created startup cards UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,39 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{query?: string}>
 }) {
 
   const query = (await searchParams).query; 
+
+  // later get posts data directly from Sanity
+  const posts = [{
+    _createdAt: new Date(),
+    views: 69,
+    author: {
+      _id : 1, 
+      name: "John Grimshaw"
+    }, 
+    _id: 1,
+    description: "The first of his name, Startup 1.", 
+    image: "https://cdn.dribbble.com/userupload/21196589/file/original-761d401b5dfba165790ec9b62b4572d6.jpg?resize=752x&vertical=center",
+    category: "Robots",
+    title: "I, Robot" 
+  },{
+    _createdAt: new Date(),
+    views: 100,
+    author: {
+      _id : 2,
+      name: "Igris Swordwind"
+    }, 
+    _id: 2,
+    description: "BallBuster FC has launched their new app!", 
+    image: "https://media.craiyon.com/2025-07-24/PxfbCz3pQNqMpli36oCANg.webp",
+    category: "Sports",
+    title: "The Ball Busters FC" 
+  }]
 
   return (
     <div className="w-screen">
@@ -16,6 +44,30 @@ export default async function Home({ searchParams }: {
 
         <SearchForm query={query}/>
 
+      </section>
+
+      <section className="section_container"> 
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}" :` : "All startups :"}
+        </p>
+
+        <ul className="mt-7 card_grid">  
+          {
+            posts?.length > 0 ? (
+              posts.map(
+              (post: StartupCardType) => {
+              
+                return (
+                  <StartupCard key={post?._id} post={post}/>
+                )
+            }
+            )
+            ): (
+              <p className="no-result">"No startups found"</p> 
+            )
+          }
+        </ul>
+        
       </section>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    dangerouslyAllowSVG: true,   // will remove later, added only for placeholder
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "*"
+    }]
+  }
 };
 
 export default nextConfig;
